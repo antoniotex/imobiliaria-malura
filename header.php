18257b5e674f9174f83e5c5f8225f1b3aaee327d
unknown <antoniotx.dev@gmail.com>
2019-04-07 20:51:40 -0300

    Add suporte a thumbnails e alguns css

--- header.php
+++ header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+  <?php $home = get_template_directory_uri();?>
   <?php wp_head()?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="<?= $home ?>/reset.css" />
+  <link rel="stylesheet" type="text/css" href="<?= $home ?>/style.css" />
   <title>Document</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -3,20 +3,29 @@
   get_header()
 ?>
 
-<h1>Bem Vindos!</h1>
-
-<?php
-  if(have_posts()){
-    while(have_posts()){
-      the_post();
-?>
-<h2><?php the_title()?></h2>
-<p><?php the_content()?></p>
-
-<?php
-    }
-  }
-?>
+<main>
+  <div class="home-main">
+    <div class="container">
+    <h1>Bem Vindo ao Maluras</h1>
+      <ul class="imoveis-listagem">
+        <?php
+          if(have_posts()){
+            while(have_posts()){
+              the_post();
+        ?>
+        <li class="imoveis-listagem-item">
+          <?php the_post_thumbnail()?>
+          <h2><?php the_title()?></h2>
+          <p><?php the_content()?></p>
+        </li>
+        <?php
+            }
+          }
+        ?>
+      </ul>
+    </div>
+  </div>
+</main>
 
 <?php 
   // include('footer.php') Pode ser assim
