e4f23b8c59665c5db28b4eac2840a11da0617498
unknown <antoniotx.dev@gmail.com>
2019-04-07 19:50:45 -0300

    Adiciona metodo para imprimir posts

--- index.php
+++ index.php
@@ -2,7 +2,19 @@
   // include('header.php') Pode ser assim
   get_header()
 ?>
-  <h1>Bem Vindos!</h1>
+
+<h1>Bem Vindos!</h1>
+
+<?php
+  if(have_posts()){
+    while(have_posts()){
+      the_post();
+      the_title();
+      the_content();
+    }
+  }
+?>
+
 <?php 
   // include('footer.php') Pode ser assim
   get_footer()
